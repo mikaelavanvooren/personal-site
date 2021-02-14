ba02b3efbbb87b35efdd658649267b43d0c3e028
Mikaela <mika1@miadolce.com>
2021-02-14 14:34:33 -0800

    Fleshed Out Page

--- src/App.js
+++ src/App.js
@@ -15,51 +15,49 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 function App() {
   return (
     <div className="App">
-      	
-      		<Container fluid id={"imgContainer"}>
-      			<Row >
-      				<Col>
-  							<Image src="PersonalPhoto.jpeg" id={"personalPhoto"} fluid/>
-  						</Col>
-  						<Col>
-  							<Card square id={"textBox"}>
-  								<div id={"aboutText"}>Hi, my name is Mikaela Van Vooren. <br/> <br/>
-  								I believe software engineers create specialized tools to solve problems. I consider myself primarily a problem solver who uses software engineering as one of the many tools in my arsenal to get the job done. <br/><br/><br/>
-  								</div>
-  								<IconButton aria-label="linkedin" href="http://linkedin.com/in/mikaelavanvooren/">
-  									<LinkedInIcon />
-									</IconButton>
-									<IconButton aria-label="github" href="http://github.com/mikaelavanvooren">
-  									<GitHubIcon />
-									</IconButton> 
-									<IconButton aria-label="email" href={"mailto:mikaelavanvooren@gmail.com"}>
-  									<MailOutlineIcon />
-									</IconButton> 
-  							</Card>
-  						</Col>
-  					</Row>
-  				</Container>
-  				<Container fluid id={"infoContainer"}>
+  		<Container fluid id={"imgContainer"}>
+  			<Row >
+  				<Col>
+						<Image src="PersonalPhoto.jpeg" id={"personalPhoto"} fluid/>
+					</Col>
+					<Col>
+						<Card square id={"textBox"}>
+							<div id={"aboutText"}>Hi, my name is Mikaela Van Vooren. <br/> <br/>
+								I believe software engineers create specialized tools to solve problems. I consider myself primarily a problem solver who uses software engineering as one of the many tools in my arsenal to get the job done. <br/><br/> 
+							</div>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+			<Container fluid id={"infoContainer"}>
+			  <div id={"aboutMe"}>problems</div>
+			  	<Container className="aboutText">
+					  <div>Some of the problems I'm most passionate about working on are supporting those with <b>mental heath struggles</b>, finding new ways to further <b>environmental sustainability</b> and developing new techniques to get kids excited about <b>STEAM subjects in school</b>
+			  		</div>
+			  	</Container>
+				</Container>
+				<Container fluid id={"projectsContainer"}>
+				  <div id={"projects"}>projects</div>
+				  	<Container className="aboutText">
+						  <div>I've worked on projects using C++, Java, React, Node, Javascript and HTML/CSS
+				  		</div>
+			  	</Container>
+			  </Container>
 
-			      <Grid container justify="center" spacing={4}>
-			        	<Grid item xs={3}>
-			        		<Paper>
-			  						<div>Some of the problems I'd love to work on include supporting those with <b>mental heath struggles</b>, finding new ways to further <b>environmental sustainability</b> and developing new techniques to get kids excited about <b>STEAM subjects in school</b>.</div>
-			  					</Paper>
-			  				</Grid>
-			  				<Grid item xs={3}>
-			        		<Paper>
-			  						<div>Technologies I've worked with include:</div>
-			  					</Paper>
-			  				</Grid>
-			  				<Grid item xs={3}>
-			        		<Paper>
-			  						<div>Test</div>
-			  					</Paper>
-			  				</Grid>
-							</Grid>
-						</Container>
-    </div>
+				<Container fluid id={"footer"}>
+					<div>Feel free to reach out!<br/>
+					</div>
+					<IconButton aria-label="linkedin" href="http://linkedin.com/in/mikaelavanvooren/">
+						<LinkedInIcon />
+					</IconButton>
+					<IconButton aria-label="github" href="http://github.com/mikaelavanvooren">
+						<GitHubIcon />
+					</IconButton> 
+					<IconButton aria-label="email" href={"mailto:mikaelavanvooren@gmail.com"}>
+						<MailOutlineIcon />
+					</IconButton> 
+				</Container>
+    	</div>
   );
 }
 
