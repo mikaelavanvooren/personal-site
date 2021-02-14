922561bf07efcfcdd5130fe567270b30220a6691
Mikaela <mika1@miadolce.com>
2021-02-13 21:13:08 -0800

    Added Name and Cleaned Up Old imgs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      	<div className="heading">
+        	<p className="main-name">Mikaela Van Vooren</p>
+        	<p className="major">Software Engineering Undergraduate</p>
+        </div>
+      <Grid container justify="center" spacing={2}>
+        	<Grid item xs={3}>
+        		<Paper>
+  						<div>Test</div>
+  					</Paper>
+  				</Grid>
+  				<Grid item xs={3}>
+        		<Paper>
+  						<div>Test</div>
+  					</Paper>
+  				</Grid>
+  				<Grid item xs={3}>
+        		<Paper>
+  						<div>Test</div>
+  					</Paper>
+  				</Grid>
+				</Grid>
     </div>
   );
 }
