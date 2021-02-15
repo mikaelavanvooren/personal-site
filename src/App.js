67ae219a7e2a3660062d4fd7379eb753bb4aa148
Mikaela <mika1@miadolce.com>
2021-02-14 17:28:09 -0800

    Made More Mobile Friendly
    Switched px to either em or vh/vw so it’s more responsive.

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
   return (
     <div className="App">
   		<Container fluid id={"imgContainer"}>
-  			<Row >
-  				<Col>
+  			<Row>
+  				<Col >
 						<Image src="/PersonalPhoto.jpeg" id={"personalPhoto"} fluid/>
 					</Col>
-					<Col>
-						<div id={"aboutText"}><h2><b>Hi, my name is Mikaela Van Vooren.</b></h2> <br/>
-							I believe software engineers create specialized tools to solve problems. I consider myself primarily a problem solver who uses software engineering as one of the many tools in my arsenal to get the job done. <br/> 
+					<Col sm={6} xs={12}>
+						<div id={"aboutBio"}><h3><b>Hi, my name is Mikaela Van Vooren.</b></h3> <br/>
+							I believe software engineers create specialized tools to solve problems. I consider myself primarily a problem solver who uses software engineering as one of the many tools in my arsenal to get the job done. <br/> <br/> 
 						</div>
 						<IconButton aria-label="linkedin" href="http://linkedin.com/in/mikaelavanvooren/">
 							<LinkedInIcon />
@@ -37,9 +37,9 @@ function App() {
 				</Row>
 			</Container>
 			<Container fluid id={"infoContainer"}>
-			  <div id={"aboutMe"}>problems</div>
+			  <div id={"problems"}>problems</div>
 			  	<Container className="aboutText">
-					  <div><br/><br/><br/>Some of the problems I'm most passionate about are finding ways to support those with <b>mental heath struggles</b>, working on new technology to further <b>environmental sustainability</b> and developing new techniques to get kids excited about <b>STEAM subjects in school</b>. <br/><br/>I'm excited about where we will lead the future with what we are doing now to drive it forward!
+					  <div><br/><br/>Some of the problems I'm most passionate about are finding ways to support those with <b>mental heath struggles</b>, working on new technology to further <b>environmental sustainability</b> and developing new techniques to get kids excited about <b>STEAM subjects in school</b>. <br/><br/>I'm excited about where we will lead the future with what we are doing now to drive it forward!
 					  <br/><br/><br/>
 			  		</div>
 			  	</Container>
@@ -47,7 +47,7 @@ function App() {
 				<Container fluid id={"projectsContainer"}>
 				  <div id={"projects"}>technology</div>
 				  	<Container className="aboutText">
-						  <div>At my time in school I've been exposed to many different technologies from Assembly to React.js. Each has had their pros and their cons but there's always been something about backend development and figuring out how to orginize data in more efficent ways that I find appealing. Embedded systems is also fascinating to me because I love seeing how we get from literal 0s and 1s to everything that we consider technology now.
+						  <div><br/><br/>At my time in school I've been exposed to many different technologies from Assembly to React.js. Each has had their pros and their cons but there's always been something about backend development and figuring out how to orginize data in more efficent ways that I find appealing. Embedded systems is also fascinating to me because I love seeing how we get from literal 0s and 1s to everything that we consider technology now.
 
 						  <br/><br/>Languages Used: Java, React, Node, MIPS, Javascript and HTML/CSS
 				  		</div>
